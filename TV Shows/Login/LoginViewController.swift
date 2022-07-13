1928056cda1ad140f57d17590e1f7244bf623618
Ivana Rajic <ivanarajic.zg@gmail.com>
2022-07-13 13:41:26 +0200

    added new storyboard, view controller, button and label

---
+++ TV Shows/Login/LoginViewController.swift
@@ -0,0 +1,12 @@
+//
+//  LoginViewController.swift
+//  TV Shows
+//
+//  Created by Infinum on 7/13/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+}
--- TV Shows/TV Shows/AppDelegate.swift
+++ TV Shows/TV Shows/AppDelegate.swift
@@ -7,7 +7,6 @@
 
 import UIKit
 
-@main
 
 
 
@@ -18,7 +17,7 @@ import UIKit
 
     // MARK: UISceneSession Lifecycle
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
@@ -30,6 +29,4 @@ import UIKit
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
-}
 

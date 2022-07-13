e029390883d463f35b132243ea52b92ea153fb94
Ivana Rajic <ivanarajic.zg@gmail.com>
2022-07-14 00:01:01 +0200

    assignment complete

--- TV Shows/Login/LoginViewController.swift
+++ TV Shows/Login/LoginViewController.swift
@@ -5,12 +5,23 @@
 //  Created by Infinum on 7/13/22.
 //
 
+var numberOfClicks = 0
+
 import UIKit
 
 class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hello world")
     }
+    
+    @IBAction func ButtonActionHendler(_ sender: Any) {
+        print("Click, click")
+        numberOfClicks+=1
+        self.LabelOutlet.text = "\(numberOfClicks)"
+        
+    }
+    
+    @IBOutlet weak var LabelOutlet: UILabel!
+    
 }

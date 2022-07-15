8eaeec89f054246be43df86052b569eb70be5fc3
Ivana Rajic <ivanarajic.zg@gmail.com>
2022-07-15 19:18:55 +0200

    added private modifier to variable and final to class, removing sender from func buttonActionHandler sends error "Unrecognized selector sent to instance"

--- TV Shows/Login/LoginViewController.swift
+++ TV Shows/Login/LoginViewController.swift
@@ -2,26 +2,24 @@
 //  LoginViewController.swift
 //  TV Shows
 //
-//  Created by Infinum on 7/13/22.
+//  Created by Infinum on 7/15/22.
 //
-
-var numberOfClicks = 0
 
 import UIKit
 
-class LoginViewController: UIViewController {
+final class LoginViewController: UIViewController {
+    
+    private var numberOfTaps:Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    @IBAction func ButtonActionHendler(_ sender: Any) {
-        print("Click, click")
-        numberOfClicks+=1
-        self.LabelOutlet.text = "\(numberOfClicks)"
-        
+    @IBAction private func buttonAction(_ sender: Any) {
+        numberOfTaps+=1
+        self.labelOutlet.text="\(numberOfTaps)"
     }
     
-    @IBOutlet weak var LabelOutlet: UILabel!
+    @IBOutlet weak var labelOutlet: UILabel!
     
 }

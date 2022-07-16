f6841b47911b7c73b8471f2fcc7e8a1e2555e2b5
Ivana Rajic <ivanarajic.zg@gmail.com>
2022-07-16 20:30:43 +0200

    colored the view controller, added image to button (round edges and color included), changed font and size of the label

--- TV Shows/Login/LoginViewController.swift
+++ TV Shows/Login/LoginViewController.swift
@@ -12,14 +12,31 @@ final class LoginViewController: UIViewController {
     private var numberOfTaps:Int = 0
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        self.viewOutlet.backgroundColor = UIColor.gray
+        
+        self.button.backgroundColor = .clear
+        self.button.layer.cornerRadius = 5
+        self.button.layer.borderWidth = 1
+        self.button.layer.borderColor = UIColor.black.cgColor
+        
+        self.labelOutlet.textColor = UIColor.darkGray
+        self.labelOutlet.font = UIFont(name: "HelveticaNeue", size: 30)
+        
     }
+    
     
     @IBAction private func buttonAction(_ sender: Any) {
+        
         numberOfTaps+=1
         self.labelOutlet.text="\(numberOfTaps)"
+        
     }
+
     
     @IBOutlet weak var labelOutlet: UILabel!
+    @IBOutlet var viewOutlet: UIView!
+    @IBOutlet weak var button: UIButton!
     
 }

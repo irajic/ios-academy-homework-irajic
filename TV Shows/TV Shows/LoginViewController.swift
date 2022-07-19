8d1dc10ceaa84cc0393c854c85226511534e48b9
Ivana Rajic <ivanarajic.zg@gmail.com>
2022-07-20 01:01:26 +0200

    finishing login screen layout

--- TV Shows/TV Shows/LoginViewController.swift
+++ TV Shows/TV Shows/LoginViewController.swift
@@ -13,8 +13,7 @@ final class LoginViewController: UIViewController {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var stackView: UIStackView!
     @IBOutlet weak var labelView: UIView!
-    
-    // MARK: -Properties
+    @IBOutlet weak var checkboxButton: UIButton!
     
     
     // MARK: -Lifecycle methodes
@@ -24,6 +23,7 @@ final class LoginViewController: UIViewController {
     }
     
     // MARK: -Methodes
+    
     private func createStackView() {
         
         let bigLabel = UILabel(frame: CGRect(x: 20,y: 188, width: 120, height: 41))
@@ -36,15 +36,51 @@ final class LoginViewController: UIViewController {
         smallLabel.textColor = .white
         smallLabel.adjustsFontSizeToFitWidth = true
         
+        let rememberMeLabel = UILabel(frame: CGRect(x: 52, y: 424, width: 300, height: 30))
+        rememberMeLabel.text = "Remember me"
+        rememberMeLabel.textColor = .white
+        rememberMeLabel.adjustsFontSizeToFitWidth = true
+        
+        let loginButton = UIButton(frame: CGRect(x: 39, y: 485, width: 312, height: 45))
+        loginButton.backgroundColor = .white
+        loginButton.layer.cornerRadius = 15
+        loginButton.layer.borderWidth = 1
+        loginButton.layer.borderColor = UIColor.clear.cgColor
+        loginButton.setTitle("Login", for: .normal)
+        loginButton.setTitleColor(UIColor.blue, for: .normal)
+        
+        
+        let registerButton = UIButton(frame: CGRect(x: 138, y: 551, width: 109, height: 20))
+        registerButton.backgroundColor = UIColor.clear
+        registerButton.setTitleColor(UIColor.white, for: .normal)
+        registerButton.setTitle("Register", for: .normal)
+        
         let labelView = UIView()
         labelView.addSubview(bigLabel)
         labelView.addSubview(smallLabel)
+        labelView.addSubview(rememberMeLabel)
+        
+        let buttonView = UIView()
+        buttonView.addSubview(loginButton)
+        buttonView.addSubview(registerButton)
         
         
-        let stackView = UIStackView(arrangedSubviews: [labelView])
+        let stackView = UIStackView(arrangedSubviews: [labelView, buttonView])
         stackView.axis = .vertical
         scrollView.addSubview(stackView)
         
+    }
+    
+    // MARK: -Actions
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        if(sender.isSelected == true) {
+            sender.setBackgroundImage(UIImage(named: "checkmark.rectangle"), for: UIControl.State.selected)
+            sender.isSelected = false
+        }
+        else {
+            sender.setBackgroundImage(UIImage(named: "rectangle"), for: UIControl.State.normal)
+            sender.isSelected = true
+        }
     }
     
 }

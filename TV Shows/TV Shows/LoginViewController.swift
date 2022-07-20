37f7e1cd837cd0d491623bc6755f7dfdd317ab29
Ivana Rajic <ivanarajic.zg@gmail.com>
2022-07-21 00:49:01 +0200

    api calls added

--- TV Shows/TV Shows/LoginViewController.swift
+++ TV Shows/TV Shows/LoginViewController.swift
@@ -12,6 +12,8 @@ import Alamofire
 final class LoginViewController: UIViewController {
     
     // MARK: -Outlet
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     
     // MARK: -Lifecycle methodes
     override func viewDidLoad() {
@@ -26,21 +28,105 @@ final class LoginViewController: UIViewController {
         sender.isSelected.toggle()
     }
     
-    @IBAction func loginButtonClicked(_ sender: Any) {
-        
-        let homeStoryboard = UIStoryboard(name: "Home", bundle: nil)
-        let homeViewController = homeStoryboard.instantiateViewController(withIdentifier: "Home") as! HomeViewController
-        navigationController?.pushViewController(homeViewController, animated: true)
-        
-    }
-    
     @IBAction func registerButtonClicked(_ sender: Any) {
         
-        let homeStoryboard = UIStoryboard(name: "Home", bundle: nil)
-        let homeViewController = homeStoryboard.instantiateViewController(withIdentifier: "Home") as! HomeViewController
-        navigationController?.pushViewController(homeViewController, animated: true)
+        let registerParameters: [String: String] = [
+            "email": emailTextField.text!,
+            "password": passwordTextField.text!,
+            "password_confirmation": passwordTextField.text!
+        ]
         
+        MBProgressHUD.showAdded(to: self.view, animated: true)
+        if emailTextField.text!.isEmpty {
+            print("error")
+        } else {
+            AF
+                .request ("https://tv-shows.infinum.academy/users",
+                          method: HTTPMethod.post,
+                          parameters: registerParameters,
+                          encoder: JSONParameterEncoder.default
+                )
+                .responseJSON { [weak self] response in
+                    guard let self = self else {
+                        return
+                    }
+                    MBProgressHUD.hide(for: self.view, animated: true)
+                    switch response.result {
+                    case .success(let userInfo):
+                        let user = User(params: userInfo)
+                        print(user?.email)
+                    
+                        let homeStoryboard = UIStoryboard(name: "Home", bundle: nil)
+                        let homeViewController = homeStoryboard.instantiateViewController(withIdentifier: "Home") as! HomeViewController
+                        self.navigationController?.pushViewController(homeViewController, animated: true)
+                    
+                    case .failure(let error):
+                        print(error)
+                    }
+                }
+        }
+}
+    
+@IBAction func loginUser(_ sender: Any) {
+        
+        let loginParameters: [String: String] = [
+            "email": emailTextField.text!,
+            "password": passwordTextField.text!
+        ]
+        
+        MBProgressHUD.showAdded(to: self.view, animated: true)
+        
+        AF
+            .request(
+                "https://tv-shows.infinum.academy/users/sign_in",
+                method: HTTPMethod.post,
+                parameters: loginParameters,
+                encoder: JSONParameterEncoder.default
+            )
+            .validate()
+            .responseJSON { [weak self] response in
+                guard let self = self else {
+                    return
+                }
+                MBProgressHUD.hide(for: self.view, animated: true)
+                switch response.result {
+                case .success(let userInfo):
+                    let user = User(params: userInfo)
+                    print(user?.email)
+                    
+                    let homeStoryboard = UIStoryboard(name: "Home", bundle: nil)
+                    let homeViewController = homeStoryboard.instantiateViewController(withIdentifier: "Home") as! HomeViewController
+                    self.navigationController?.pushViewController(homeViewController, animated: true)
+                    
+                case .failure(let error):
+                    print(error)
+            }
+        }
     }
     
+}
+
+struct User {
+    
+    let email: String
+    let id: String
+    let image_url: String?
+    
+    init?(params: Any) {
+        let dict = params as? [String: Any]
+        let user = dict?["user"] as? [String: Any]
+        let email = user?["email"] as? [String: Any]
+        
+        let parsedEmail = user?["email"] as? String
+        let parsedId = user?["id"] as? String
+        self.image_url = user? ["image_url"] as? String
+        
+        if let parsedEmail = parsedEmail, let parsedId = parsedId {
+            self.email = parsedEmail
+            self.id = parsedId
+        } else {
+            return nil
+        }
+    }
     
 }
